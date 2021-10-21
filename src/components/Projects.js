7468b68c99f6ed1ac2cf3d30a47d8055822b9ce1
Austin Parker <austincparker@gmail.com>
2021-10-21 09:45:01 -0500

    routes working

---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// export default function Projects({ project }) {
+//     return (
+//         <div>
+//             {project.name}
+//         </div>
+//     )
+// }
---
+++ src/views/ProjectView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProjectView() {
+  return (
+    <div>
+      <h1>Projects</h1>
+    </div>
+  );
+}
