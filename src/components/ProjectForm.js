496f334efbee1dd12ddb3e191d64aaf89f7829ff
Austin Parker <austincparker@gmail.com>
2021-11-17 16:28:13 -0600

    project form complete

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router';
 
 const initialState = {
+  firebaseKey: '',
   name: '',
   languages: '',
   description: '',
   imageUrl: '',
-  firebaseKey: '',
   appLink: '',
+  githubRepo: '',
   otherTools: '',
 };
 
@@ -47,18 +48,135 @@ export default function ProjectForm({ obj }) {
   return (
     <div>
       <h1>Project Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input
-            id="projectName"
-            name="projectName"
-            value={formInput.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <button type="submit">Create</button>
-      </form>
+      <div
+        className="card-body"
+        style={{ justifyContent: 'space-between', border: '2px solid black' }}
+      >
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div>
+              <label htmlFor="name">
+                Project Name
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="name"
+                  name="name"
+                  value={formInput.name}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="languages">
+                Languages
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="languages"
+                  name="languages"
+                  value={formInput.languages}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="description">
+                Description
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="description"
+                  name="description"
+                  value={formInput.description}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="imageUrl">
+                Image URL
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="imageUrl"
+                  name="imageUrl"
+                  value={formInput.imageUrl}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="appLink">
+                App Link
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="appLink"
+                  name="appLink"
+                  value={formInput.appLink}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="githubRepo">
+                GitHub Repo Link
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="githubRepo"
+                  name="githubRepo"
+                  value={formInput.githubRepo}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="otherTools">
+                Other Tools
+                <input
+                  style={{
+                    border: '2px solid black',
+                    height: '2.5rem',
+                    width: '30rem',
+                  }}
+                  id="otherTools"
+                  name="otherTools"
+                  value={formInput.otherTools}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
