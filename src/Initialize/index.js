69a81d61c9fa9284d25a20dc4a9af8f42308b845
Austin Parker <austincparker@gmail.com>
2021-11-16 21:02:11 -0600

    moved user state to create

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import React from 'react';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
-import SignIn from '../views/SignIn';
-import SignOut from '../views/SignOut';
 
 function Initialize() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((authed) => {
-      if (authed) {
-        const userInfoObj = {
-          fullName: authed.displayName,
-          uid: authed.uid,
-        };
-        setUser(userInfoObj);
-      } else if (user || user === null) {
-        setUser(false);
-      }
-    });
-  }, []);
-
   return (
     <div className="App container">
-      {user ? (
-        <>
-          <Navigation />
-          <Routes />
-          <SignOut />
-        </>
-      ) : (
-        <>
-          <Navigation />
-          <Routes />
-          <SignIn user={user} />
-        </>
-      )}
+      <Navigation />
+      <Routes />
     </div>
   );
 }
