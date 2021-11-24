494a344ac9ac9370e0faa69d0b4c874cfccb0abc
Austin Parker <austincparker@gmail.com>
2021-11-23 20:55:25 -0600

    added to intro

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const OuterContainer = styled.div`
-  .prof-pic {
+  .prof-pic-bkg {
     background: url(../../../imgs/acp-background.png);
     padding: 33px 0 33px 0;
-    img {
+    .prof-pic {
       width: 300px;
       border-radius: 50%;
+    }
+    .hobbies {
+      color: white;
+      text-align: right;
+      margin-right: 40px;
     }
   }
 `;
@@ -15,9 +20,20 @@ const OuterContainer = styled.div`
 export default function Home() {
   return (
     <OuterContainer className="text-center">
-      <div className="prof-pic rounded-bottom">
-        <img src="../../../imgs/austin-parker.png" alt="austin-parker" />
+      <div className="prof-pic-bkg rounded-bottom justify-content-between d-flex flex-wrap align-items-center">
+        <img
+          src="../../../imgs/austin-parker.png"
+          alt="austin-parker"
+          className="prof-pic"
+        />
+        <div className="hobbies">
+          <h2>Musician</h2>
+          <h2>Mountain Biker</h2>
+          <h2>Kombucha Brewer</h2>
+          <h2>Software Engineer</h2>
+        </div>
       </div>
+      <div className="type-writer d-flex flex-wrap justify-content-center" />
     </OuterContainer>
   );
 }
